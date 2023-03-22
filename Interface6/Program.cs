f75e17f42b43719f6de138259aa1cba3050aff80
Ahmet <Ahmet@DESKTOP-GM6KUI1>
2023-03-22 21:30:56 +0300

    update

---
+++ Interface6/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Interface6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            Console.ReadLine();
+        }
+    }
+    class Satici : IKisi
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public int VergiNo { get; set; }
+        public string Bolge { get; set; }
+        public string TcNo { get; set; }
+    }
+    class Alici : IKisi
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string TcNo { get; set; }
+    }
+    class Kargocu : IKisi, ITasiyabilirlik
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string Bolge { get; set; }
+        public Arac Araci { get; set; }
+
+        public void TasimaKapasitesi(Urun urun)
+        {
+            if (urun.Agirlik > 10)
+            {
+                Console.WriteLine("Ağırlık 10 kilodan fazla olduğundan kargoyu araba ile gönder");
+            }
+            else if (urun.Agirlik > 5)
+            {
+                Console.WriteLine("Ağırlık 5 kilodan fazla olduğundan kargoyu motorsiklet ile gönder");
+            }
+            else
+            {
+                Console.WriteLine("Ağırlık 5 kilodan az olduğundan kargoyu bisiklet ile gönder");
+            }
+        }
+
+    }
+
+    interface ITasiyabilirlik
+    {
+        public void TasimaKapasitesi(Urun urun);
+
+    }
+    class Arac
+    {
+        public static string AracTuru { get; set; }
+    }
+    class Urun
+    {
+        public string UrunAdi { get; set; }
+        public string Turu { get; set; }
+        public int Fiyat { get; set; }
+        public int Agirlik { get; set; }
+
+    }
+    interface IKisi
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+    }
+
+}
---
+++ Interface7/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Interface7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Oyuncu oyuncu1 = new Oyuncu();
+            oyuncu1.Ad = "Davargüden";
+            oyuncu1.Yas = 45;
+            Kanas kanas1 = new Kanas();
+            kanas1.Ad = "Kanas1";
+            //oyuncu1.Silahi = new Kanas();
+            oyuncu1.Silahi = kanas1;
+            Silah AK47 = new Kanas();
+
+
+            Oyuncu oyuncu2 = new Oyuncu();
+            oyuncu2.Ad = "Davarseven";
+            oyuncu2.Yas = 40;
+
+            
+
+            oyuncu1.Saldir(oyuncu2);
+
+            Oyuncu oyuncu3 = new Oyuncu();
+            oyuncu3.Ad = "Davaryiyen";
+            oyuncu3.Yas = 30;
+
+            oyuncu2.Saldir(oyuncu3);
+            oyuncu3.Saldir(oyuncu1);
+            
+            Console.ReadLine();
+        }
+    }
+
+    class Oyuncu
+    {
+        protected int can = 100;
+        public string Ad { get; set; }
+        public string NickName { get; set; }
+        public int Yas { get; set; }
+        public string Milleti { get; set; }
+        public Silah Silahi { get; set; }
+
+        public string NisanAl()
+        {
+            return null;//Nişan alma işlemleri
+        }
+        public void Saldir(Oyuncu oyuncu)
+        {
+
+            if (oyuncu.can > 0)
+            {
+                oyuncu.can--;
+                Console.WriteLine($"{oyuncu.Ad} adlı oyuncuya saldırdın yeni canı {oyuncu.can} ");
+             
+            }
+            else
+            {
+                Console.WriteLine("Düşman öldürüldü");
+            }
+        }
+    }
+
+    class Silah
+    {
+        public string Ad { get; set; }
+        public int Vurus { get; set; }
+        public double Agirlik { get; set; }
+        public int Mesafe { get; set; }
+    }
+    class AtesliSilahlar : Silah
+    {
+        public int MaksimumMermiMiktar { get; set; }
+        public void SarjorDegistir(int adet)
+        {
+            while (MaksimumMermiMiktar == 0)
+            {
+                MaksimumMermiMiktar += adet;
+            }
+
+        }
+
+    }
+    class KesiciSilahlar : Silah
+    {
+        public float Dayaniklilik { get; set; }
+
+    }
+
+    interface IYakinlastirilabilir
+    {
+        string yakinlastir();
+    }
+
+    class Kanas : AtesliSilahlar, IYakinlastirilabilir
+    {
+        public string yakinlastir()
+        {
+            return "%50 yakınlaştır";
+        }
+    }
+}
